364e3934ac40ec6e4f6f4d29635428d648b48648
Peder Sandvoll <peder.sandvoll@netpower.no>
2025-10-28 10:12:40 +0100

    feat: systemd subscribe to events

--- internal/sytemd/systemd.go
+++ internal/sytemd/systemd.go
@@ -3,8 +3,10 @@ package systemd
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/coreos/go-systemd/v22/dbus"
+	"github.com/pedersandvoll/lighthouse/internal/config"
 )
 
 type SystemdClient struct {
@@ -18,11 +20,43 @@ func Connect() (*SystemdClient, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not connect to systemd: %w", err)
 	}
+	if err := conn.Subscribe(); err != nil {
+		return nil, fmt.Errorf("could not subscribe to systemd dbus event")
+	}
+
 	return &SystemdClient{Conn: conn, Ctx: ctx}, nil
 }
 
 func (s *SystemdClient) Close() {
 	s.Conn.Close()
+}
+
+func (s *SystemdClient) SubscribeUnits(resultMap *map[string]*dbus.UnitStatus, errChan chan<- error, refreshChan <-chan struct{}, duration time.Duration, units *[]config.Service) {
+	unitStatuses, errs := s.Conn.SubscribeUnits(duration)
+
+	go func() {
+		for {
+			select {
+			case statuses := <-unitStatuses:
+				for _, unit := range *units {
+					if status, ok := statuses[unit.SystemdUnit]; ok {
+						(*resultMap)[unit.Name] = status
+					}
+				}
+			case <-refreshChan:
+				for _, unit := range *units {
+					status, _ := s.GetUnitStatus(unit.SystemdUnit)
+					if status != nil {
+						(*resultMap)[unit.Name] = status
+					}
+				}
+			case err := <-errs:
+				if errChan != nil {
+					errChan <- err
+				}
+			}
+		}
+	}()
 }
 
 func (s *SystemdClient) GetUnitStatus(unit string) (*dbus.UnitStatus, error) {
