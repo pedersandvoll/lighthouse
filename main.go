e1cf8b1d236ee54ca01d8c09e71404de17238184
Peder Sandvoll <peder.sandvoll@netpower.no>
2025-10-28 10:13:50 +0100

    feat: implement changes to systemd and config

--- main.go
+++ main.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 
+	"github.com/coreos/go-systemd/v22/dbus"
 	"github.com/pedersandvoll/lighthouse/internal/config"
 	systemd "github.com/pedersandvoll/lighthouse/internal/sytemd"
 )
 
 func main() {
-	config, err := config.Get()
+	var conf config.Config
+	refreshChan := make(chan struct{})
+	err := config.Get(&conf, refreshChan)
 	if err != nil {
 		panic(err)
 	}
@@ -19,11 +23,23 @@ func main() {
 	}
 	defer s.Close()
 
-	for _, unit := range config.Services {
-		us, err := s.GetUnitStatus(unit.SystemdUnit)
-		if err != nil {
-			panic(err)
+	units := make(map[string]*dbus.UnitStatus)
+	errChan := make(chan error)
+	duration := time.Second * 2
+	s.SubscribeUnits(&units, errChan, refreshChan, duration, &conf.Services)
+	s.UnitsCleanup(&units, &conf.Services, duration/2)
+
+	go func() {
+		for err := range errChan {
+			fmt.Printf("Error: %v\n", err)
 		}
-		fmt.Printf("Name: %s | Status: %s\n", us.Name, us.ActiveState)
+	}()
+	time.Sleep(2 * time.Second)
+	for {
+		fmt.Println("--- Units ---")
+		for _, unit := range units {
+			fmt.Printf("Name: %s | Status: %s\n", unit.Name, unit.ActiveState)
+		}
+		time.Sleep(duration)
 	}
 }
